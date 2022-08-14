c3b417bbcc11188321f6294cfefda1e0a0295f7b
John Lewis <john.lewis@cfa.harvard.edu>
2022-08-14 12:08:56 -0700

    add job handling

--- carta_start.sh
+++ carta_start.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/bash
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+# job handling from https://spin.atomicobject.com/2017/08/24/start-stop-bash-background-process/
+
+squeue -u joarlewi | grep carta
+
 APPIMAGE_EXTRACT_AND_RUN=1
 export APPIMAGE_EXTRACT_AND_RUN
-$HOME/CARTA-v2.0-redhat.AppImage --no_browser --idle_timeout 1000
+$HOME/CARTA-v2.0-redhat.AppImage --no_browser # >/dev/null 2>&1 &
+
+sleep 2s
+
+# get line of interest
+# get last entry
+# put new line in front of http
+# get lines beginning with http
+# get the line upto a space
+# keep only unique lines
+grep "CARTA is accessible at" $HOME/.carta/log/carta.log | tail -1 | sed 's/http/\nhttp/g' | grep ^http | sed 's/\(^http[^ <]*\)\(.*\)/\1/g' | sort -u
+# grep "joarlewi" carta.log | grep "carta"
+
+wait

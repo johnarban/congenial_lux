7fd15db79f8b75db1478d913448827afdf38e111
John Lewis <john.lewis@cfa.harvard.edu>
2022-08-24 10:55:45 -0700

    many changes

--- carta_start.sh
+++ carta_start.sh
@@ -8,7 +8,8 @@ squeue -u joarlewi | grep carta
 
 APPIMAGE_EXTRACT_AND_RUN=1
 export APPIMAGE_EXTRACT_AND_RUN
-$HOME/CARTA-v2.0-redhat.AppImage --no_browser --exit_timeout 120 --initial_timeout 120 --idle_timeout 1200  >/dev/null 2>&1 &
+$HOME/CARTA-v2.0-redhat.AppImage --no_browser &
+# --exit_timeout 120 --initial_timeout 120 --idle_timeout 1200
 
 sleep 2s
 
@@ -18,7 +19,9 @@ sleep 2s
 # get lines beginning with http
 # get the line upto a space
 # keep only unique lines
+
 grep "CARTA is accessible at" $HOME/.carta/log/carta.log | tail -1 | sed 's/http/\nhttp/g' | grep ^http | sed 's/\(^http[^ <]*\)\(.*\)/\1/g' | sort -u
 # grep "joarlewi" carta.log | grep "carta"
+
 
 wait
